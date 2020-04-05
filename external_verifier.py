22b124eb19622c3a29fa66ef4b77b3955b6b3731
huayangzhao <921203405@qq.com>
2020-04-05 21:58:39 +0800

    answer model

--- external_verifier.py
+++ external_verifier.py
@@ -16,10 +16,6 @@ eval_file = './data/squad_eval.json'
 ts = json.load(open(eval_file, 'r', encoding='utf-8'))
 best_th = ts["best_exact_thresh"]
 
-for k, v in null_odds.items():
-    if v > best_th:
-        prediction[k] = ""
-
 answer_null_odds_file = './data/squad_null_odds_answer_model.json'
 answer_null_odds = json.load(open(answer_null_odds_file, 'r', encoding='utf-8'))
 
@@ -138,11 +134,36 @@ def simple_replace_with_null_odds():
           f"best_threshold: {best_threshold}")
 
 
-xargs = "python ./data/eval.py ./data/dev-v2.0.json ./data/squad_preds.json --na-prob-file ./data/squad_null_odds.json --na-prob-thresh -2.75"
-os.system(xargs)
+def eval_twice():
+    tmp_file = './data/tmp_preds.json'
+    tmp_eval_file = './data/tmp_eval.json'
+    final_preds_file = "./data/final_squad_preds.json"
+    xargs = f"python ./data/eval.py ./data/dev-v2.0.json ./data/squad_preds.json " \
+            f"--na-prob-file ./data/squad_null_odds.json --na-prob-thresh {best_th}"
+    os.system(xargs)
+    for k, v in null_odds.items():
+        if v > best_th:
+            prediction[k] = ""
+    json.dump(prediction, open(tmp_file, 'w', encoding='utf-8'))
+    xargs = f"python ./data/eval.py ./data/dev-v2.0.json {tmp_file} " \
+            f"--na-prob-file ./data/squad_null_odds_answer_model.json --out-file {tmp_eval_file}"
+    os.system(xargs)
+    new_sh = json.load(open(tmp_eval_file, 'r', encoding='utf-8'))["best_exact_thresh"]
+    for k, v in answer_null_odds.items():
+        if v > new_sh:
+            prediction[k] = ""
+    json.dump(prediction, open(final_preds_file, 'w', encoding='utf-8'))
+    xargs = f"python ./data/eval.py ./data/dev-v2.0.json {final_preds_file} "
 
-start = time.time()
-simple_replace_with_null_odds()
-xargs = "python ./data/eval.py ./data/dev-v2.0.json ./data/merge_preds.json"
-os.system(xargs)
-print(f"cost time: {time.time() - start}")
+    os.system(xargs)
+
+
+# xargs = "python ./data/eval.py ./data/dev-v2.0.json ./data/squad_preds.json --na-prob-file ./data/squad_null_odds.json --na-prob-thresh -2.75"
+# os.system(xargs)
+#
+# start = time.time()
+# simple_replace_with_null_odds()
+# xargs = "python ./data/eval.py ./data/dev-v2.0.json ./data/merge_preds.json"
+# os.system(xargs)
+# print(f"cost time: {time.time() - start}")
+eval_twice()

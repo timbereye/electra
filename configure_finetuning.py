1a10cbe4bbebd439694a0e9d00fc6732ca320b06
HuaYZhao <921203405@qq.com>
2020-04-08 17:15:38 +0800

    submit

--- configure_finetuning.py
+++ configure_finetuning.py
@@ -101,7 +101,9 @@ class FinetuningConfig(object):
 
         # default locations of data files
         self.data_dir = data_dir
-        pretrained_model_dir = os.path.join(data_dir, "models", model_name)
+        # pretrained_model_dir = os.path.join(data_dir, "models", model_name)
+        pretrained_model_dir = kwargs["pretrained_model_dir"]
+
         self.raw_data_dir = os.path.join(data_dir, "finetuning_data", "{:}").format
         self.vocab_file = os.path.join(pretrained_model_dir, "vocab.txt")
         if not tf.io.gfile.exists(self.vocab_file):
@@ -111,7 +113,7 @@ class FinetuningConfig(object):
         self.init_checkpoint = None if self.debug else pretrained_model_dir
         self.model_dir = os.path.join(pretrained_model_dir, "finetuning_models",
                                       task_names_str + "_model")
-        results_dir = os.path.join(pretrained_model_dir, "results")
+        results_dir = os.path.join(data_dir, "results")
         self.results_txt = os.path.join(results_dir,
                                         task_names_str + "_results.txt")
         self.results_pkl = os.path.join(results_dir,
@@ -121,10 +123,10 @@ class FinetuningConfig(object):
         self.qa_preds_file = os.path.join(qa_topdir, "{:}_preds.json").format
         self.qa_na_file = os.path.join(qa_topdir, "{:}_null_odds.json").format
         self.preprocessed_data_dir = os.path.join(
-            pretrained_model_dir, "finetuning_tfrecords",
+            data_dir, "finetuning_tfrecords",
             task_names_str + "_tfrecords" + ("-debug" if self.debug else ""))
         self.test_predictions = os.path.join(
-            pretrained_model_dir, "test_predictions",
+            data_dir, "test_predictions",
             "{:}_{:}_{:}_predictions.pkl").format
 
         # update defaults with passed-in hyperparameters
---
+++ submit.py
@@ -0,0 +1,44 @@
+import os
+import shutil
+import tensorflow as tf
+
+flags = tf.flags
+FLAGS = flags.FLAGS
+flags.DEFINE_string("eval_file_path", None, "evaluate file path")
+flags.DEFINE_string("answer_model_path", None, "answer model path")
+flags.DEFINE_string("qa_model_path", None, "qa model path")
+flags.DEFINE_string("output_dir", None, "output_dir")
+
+
+def check_and_mkdirs(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
+    return True
+
+
+def main():
+    output_dir = FLAGS.output_dir
+    qa_model_dir = FLAGS.qa_model_path
+    answer_model_dir = FLAGS.answer_model_path
+    check_and_mkdirs(output_dir + "_qa")
+    check_and_mkdirs(output_dir + "_answer")
+    raw_data_dir = os.path.join(output_dir, "finetuning_data", "squad")
+    check_and_mkdirs(raw_data_dir)
+    eval_file_path = FLAGS.eval_file_path
+    shutil.copy(eval_file_path, os.path.join(raw_data_dir, "dev.json"))
+
+    qa_xargs = f"""python run_finetuning.py \
+  --data-dir={output_dir}_qa \
+  --hparams '{{"model_size": "large", "task_names": ["squad"], "eval_batch_size": 16, "predict_batch_size": 16, "max_seq_length": 512, "learning_rate": 5e-5, "use_tfrecords_if_existing": false, "do_train": false, "do_eval": true, "pretrained_model_dir": {qa_model_dir} }}'
+"""
+    os.system(qa_xargs)
+
+    answer_xargs = f"""python run_finetuning.py \
+  --data-dir={output_dir}_answer \
+  --hparams '{{"model_size": "large", "task_names": ["squad"], "eval_batch_size": 16, "predict_batch_size": 16, "max_seq_length": 512, "learning_rate": 5e-5, "use_tfrecords_if_existing": false, "do_train": false, "do_eval": true, "pretrained_model_dir": {qa_model_dir} }}'
+"""
+    os.system(answer_xargs)
+
+
+if __name__ == '__main__':
+    pass

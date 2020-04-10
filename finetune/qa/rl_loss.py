80b70992ef597cdb1d44cdeb9bb4b38676741ef2
HuaYZhao <921203405@qq.com>
2020-04-10 16:36:40 +0800

    rl2

--- finetune/qa/rl_loss.py
+++ finetune/qa/rl_loss.py
@@ -128,8 +128,8 @@ def reforce_f1_ce_loss(start_logits, end_logits, start_positions, end_positions,
                                                 num_samples)
 
     gamma = tf.cond(tf.logical_and(tf.logical_not(tf.logical_or(is_em, is_no_answer)), is_contain_answer),
-                    lambda: .5 * tf.ones([bsz, ]),
-                    lambda: tf.zeros([bsz, ]))
+                    lambda: .5 * tf.ones_like(start_ce_loss),
+                    lambda: tf.zeros_like(start_ce_loss))
     start_loss = start_ce_loss * (1 - gamma) + start_rl_loss * gamma
     end_loss = end_ce_loss * (1 - gamma) + end_rl_loss * gamma
 

8378fe33c146da9f990c93905a282777e2327648
HuaYZhao <921203405@qq.com>
2020-04-14 13:13:50 +0800

    ev

--- data/eval_refine.py
+++ data/eval_refine.py
@@ -1,6 +1,7 @@
 import pickle
 import json
 from functional import seq
+import os
 import numpy as np
 from sklearn.metrics import classification_report, accuracy_score
 from finetune.qa.qa_metrics import _compute_softmax
@@ -26,4 +27,50 @@ def eval_class():
 
 
 def eval_preds():
-    pass
+    squad_preds = json.load(open('squad_preds.json', 'r', encoding='utf-8'))
+    best_th = json.load(open('squad_eval.json', 'r', encoding='utf-8'))['best_exact_thresh']
+    print("init eval:")
+    xargs = f"python eval.py dev-v2.0.json squad_preds.json " \
+            f"--na-prob-file squad_null_odds.json "
+    os.system(xargs)
+
+    all_nbest = pickle.load(open('all_nbest_file.pkl', 'rb'))
+    refine_score = pickle.load(open('dev_refine.pkl', 'rb'))
+    dev = json.load(open('dev-v2.0.json', 'r', encoding='utf-8'))['data']
+
+    for article in dev:
+        for p in article['paragraphs']:
+            for qa in p['qas']:
+                qid = qa['id']
+                true_answers = qa['answers']
+                v = refine_score[qid]
+                probs = _compute_softmax(np.sum(np.stack(v['refine_logits'], axis=-1), axis=-1).tolist())
+                refine_class = np.argmax(probs)
+                refine_class = refine_class if probs[refine_class] > 0.9 else 0
+                nbest = all_nbest[qid]
+                if refine_class == 1:
+                    select_answers = (seq(nbest)
+                                      .filter(lambda x: squad_preds[qid] in x['text'])
+                                      .sorted(lambda x: x['probability'], reverse=True)
+                                      ).list()
+
+                elif refine_class == 2:
+                    select_answers = (seq(nbest)
+                                      .filter(lambda x: x['text'] in squad_preds[qid])
+                                      .sorted(lambda x: x['probability'], reverse=True)
+                                      ).list()
+                else:
+                    continue
+                if len(select_answers) < 2:
+                    continue
+                text = select_answers[1]['text']
+                squad_preds[qid] = text
+    json.dump(squad_preds, open('squad_refine_preds.json', 'w', encoding='utf-8'))
+
+    print("refine eval:")
+    xargs = f"python eval.py dev-v2.0.json squad_refine_preds.json " \
+            f"--na-prob-file squad_null_odds.json"
+    os.system(xargs)
+
+
+eval_preds()

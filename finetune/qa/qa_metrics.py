16e14cc656f5f3e4e1a369222668adbff448f672
HuaYZhao <921203405@qq.com>
2020-04-14 10:50:16 +0800

    refine

--- finetune/qa/qa_metrics.py
+++ finetune/qa/qa_metrics.py
@@ -267,8 +267,6 @@ class SpanBasedQAScorer(scorer.Scorer):
 
         utils.write_json(dict(all_predictions),
                          self._config.qa_preds_file(self._name))
-        utils.write_json(all_nbest_json,
-                         self._config.qa_all_nbest_file(self._name))
         if self._v2:
             utils.write_json({
                 k: float(v) for k, v in six.iteritems(scores_diff_json)},
@@ -289,6 +287,7 @@ class SpanBasedQAScorer(scorer.Scorer):
 
         import pickle
         pickle.dump(refine, open('refine.pkl', 'wb'))
+        pickle.dump(all_nbest_json, open("all_nbest_file", 'wb'))
 
 
 def _get_best_indexes(logits, n_best_size):

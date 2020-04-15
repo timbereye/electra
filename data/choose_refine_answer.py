0b59f0ddb65203f3aa8968d9960ae6b30e00d10c
HuaYZhao <921203405@qq.com>
2020-04-15 13:51:56 +0800

    mc

---
+++ data/choose_refine_answer.py
@@ -0,0 +1,71 @@
+import json
+from copy import deepcopy
+import os
+import pickle
+from matplotlib import pyplot as plt
+from data.eval import get_raw_scores
+import numpy as np
+import time
+from functional import seq
+from data.eval import normalize_answer
+
+preds = json.load(open('squad_preds.json', 'r', encoding='utf-8'))
+null_odds = json.load(open('squad_null_odds.json', 'r', encoding='utf-8'))
+answer_null_odds = json.load(open('squad_null_odds_answer_model.json', 'r', encoding='utf-8'))
+
+best_th = json.load(open('squad_eval.json', 'r', encoding='utf-8'))["best_exact_thresh"]
+
+print("init eval:")
+xargs = f"python eval.py dev-v2.0.json squad_preds.json --na-prob-file squad_null_odds.json "
+os.system(xargs)
+
+answer_chooses = pickle.load(open('dev_f1_predict_results.pkl', 'rb'))
+nbest = pickle.load(open('dev_all_nbest_file.pkl', 'rb'))
+
+tmp_file = 'tmp_preds'
+tmp_eval_file = 'tmp_eval_file'
+final_preds_file = 'final_preds.json'
+
+length = 5
+believe_f1_th = 80
+believe_prob_th = 0.1
+
+for qid in preds:
+    chooses = (seq(range(length))
+               .map(lambda x: answer_chooses.get(f"{qid}_{x}", None))
+               .filter(lambda x: x)
+               .flatten()
+               )
+    if chooses.len():
+        chooses = (chooses.zip(nbest[qid][:length])
+                   .map(lambda x: {'text': x[1]['text'],
+                                   'f1_pred': x[0]['predictions'],
+                                   'prob': x[1]['probability']})
+                   .sorted(lambda x: x['f1_pred'], reverse=True)
+                   ).list()
+        max_prob = seq(chooses).map(lambda x: x['prob']).max()
+        if chooses[0]['f1_pred'] > believe_f1_th and max_prob - chooses[0]['prob'] < believe_prob_th:
+            preds[qid] = normalize_answer(chooses[0]['text'])
+
+json.dump(preds, open(final_preds_file, 'w', encoding='utf-8'))
+
+print("final eval:")
+xargs = f"python eval.py dev-v2.0.json preds_by_f1.json --na-prob-file squad_null_odds.json --out-file {tmp_eval_file}"
+os.system(xargs)
+
+for k, v in null_odds.items():
+    if v > best_th:
+        preds[k] = ""
+
+json.dump(preds, open(tmp_file, 'w', encoding='utf-8'))
+xargs = f"python eval.py dev-v2.0.json {tmp_file} " \
+        f"--na-prob-file squad_null_odds_answer_model.json --out-file {tmp_eval_file}"
+os.system(xargs)
+new_sh = json.load(open(tmp_eval_file, 'r', encoding='utf-8'))["best_exact_thresh"]
+for k, v in answer_null_odds.items():
+    if v > new_sh:
+        preds[k] = ""
+
+json.dump(preds, open(final_preds_file, 'w', encoding='utf-8'))
+xargs = f"python eval.py dev-v2.0.json {final_preds_file} "
+os.system(xargs)

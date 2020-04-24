a98f7189991ca658a3d5d3f62e770bf0f9b509f9
HuaYZhao <921203405@qq.com>
2020-04-24 11:03:17 +0800

    ev

--- retro_verifier2.py
+++ retro_verifier2.py
@@ -20,23 +20,9 @@ dev = json.load(open('./data/dev-v2.0.json', 'r', encoding='utf-8'))['data']
 retro_prediction_file = './data/retro_preds.json'
 score_diff = collections.OrderedDict()
 
-
-def get_probs(qid):
-    best = all_nbest[qid][0]
-    score_has = np.exp(best['start_logit']) + np.exp(best['start_logit'])
-    score_na = best['start_cls_logits'] + best['end_cls_logits']
-
-    y_hat = 1 / (1 + np.exp(-answer_null_odds[qid]))
-    y_ba = 1 / (1 + np.exp(-null_odds[qid]))
-
-    return [score_has, score_na, y_hat, y_ba]
-
-
-for th in np.arange(-1, 3, 0.1):
+for th in np.arange(-5, 5, 0.1):
     for qid in preds:
-        scores = get_probs(qid)
-        score_diff[qid] = scores[0] - (0.5 * scores[1] + 0.5 * (0.5 * scores[2] + 0.5 * scores[3]))
-        if score_diff[qid] < th:
+        if null_odds[qid] < th:
             preds[qid] = ""
 
     print(th)

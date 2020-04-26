a17f5b37738542d2d519ab6e300c9aae7420be63
HuaYZhao <921203405@qq.com>
2020-04-26 13:15:37 +0800

    tcn

--- model/optimization.py
+++ model/optimization.py
@@ -60,9 +60,8 @@ def create_optimizer(
     tvars = tf.trainable_variables()
     grads = tf.gradients(loss, tvars)
     (grads, _) = tf.clip_by_global_norm(grads, clip_norm=1.0)
-    grads = [g * 10 if "/tcn/" in v.name else g for v, g in
+    grads = [g * 50 if "/tcn/" in v.name else g for v, g in
              zip(tvars, grads)]
-    print(tvars)
     train_op = optimizer.apply_gradients(
         zip(grads, tvars), global_step=global_step)
     new_global_step = global_step + 1

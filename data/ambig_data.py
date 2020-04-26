807cc888a7b18094835746f9818b062d1cf2ab2a
HuaYZhao <921203405@qq.com>
2020-04-26 15:13:19 +0800

    ambig

--- data/ambig_data.py
+++ data/ambig_data.py
@@ -1,5 +1,7 @@
 import json
+from functional import seq
 
+nq_open_train = json.load(open('nqopen-train.json', 'r', encoding='utf-8'))
 ambig_train = json.load(open('train_light.json', 'r', encoding='utf-8'))
 
 print(1)

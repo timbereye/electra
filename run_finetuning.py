303066b2d28cc0377c7238a1f7c60695397e6900
HuaYZhao <921203405@qq.com>
2020-04-29 15:09:48 +0800

    at

--- run_finetuning.py
+++ run_finetuning.py
@@ -90,7 +90,7 @@ class FinetuningModel(object):
                     task_adv_losses, task_adv_outputs = task.get_prediction_module(
                         bert_model_adv, features, is_training, percent_done)
 
-                total_loss = 0.875 * task_losses + 0.125 * task_adv_losses
+                total_loss = 0.875 * task_losses + 0. * task_adv_losses
                 losses.append(total_loss)
                 self.outputs[task.name] = task_outputs
         self.loss = tf.reduce_sum(

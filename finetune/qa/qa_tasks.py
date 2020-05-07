d2f60f6415bac272c70ab01ba55eb581d6fd527c
HuaYZhao <921203405@qq.com>
2020-05-07 15:34:19 +0800

    atrl

--- finetune/qa/qa_tasks.py
+++ finetune/qa/qa_tasks.py
@@ -500,7 +500,14 @@ class QATask(task.Task):
         start_loss = compute_loss(start_logits, start_positions)
         end_loss = compute_loss(end_logits, end_positions)
 
-        losses = (start_loss + end_loss) / 2.0
+        loss_ce = (start_loss + end_loss) / 2.0
+
+        from finetune.qa.rl_loss import rl_loss
+        loss_rl = rl_loss(start_logits, end_logits, start_positions, end_positions, sample_num=4)
+        theta_ce = tf.get_variable('theta_ce', dtype=tf.float32, initializer=lambda: tf.constant(1.))
+        theta_rl = tf.get_variable('theta_rl', dtype=tf.float32, initializer=lambda: tf.constant(1.))
+        losses += (1 / (2 * theta_ce * theta_ce)) * loss_ce + (1 / (2 * theta_rl * theta_rl)) * loss_rl + tf.log(
+            theta_ce * theta_ce) + tf.log(theta_rl * theta_rl)
 
         answerable_logit = tf.zeros([batch_size])
         if self.config.answerable_classifier:

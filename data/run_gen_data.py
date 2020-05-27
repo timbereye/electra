fef8e90fda9d257d2aa7e9b93412039dc3519689
huayangzhao <921203405@qq.com>
2020-05-28 00:11:35 +0800

    submit

--- data/run_gen_data.py
+++ data/run_gen_data.py
@@ -1,6 +1,34 @@
 import json
 import pickle
 import argparse
+import re
+import collections
+import string
+
+
+def normalize_answer(s):
+    """Lower text and remove punctuation, articles and extra whitespace."""
+
+    def remove_articles(text):
+        regex = re.compile(r'\b(a|an|the)\b', re.UNICODE)
+        return re.sub(regex, ' ', text)
+
+    def white_space_fix(text):
+        return ' '.join(text.split())
+
+    def remove_punc(text):
+        exclude = set(string.punctuation)
+        return ''.join(ch for ch in text if ch not in exclude)
+
+    def lower(text):
+        return text.lower()
+
+    return white_space_fix(remove_articles(remove_punc(lower(s))))
+
+
+def get_tokens(s):
+    if not s: return []
+    return normalize_answer(s).split()
 
 
 def compute_f1(a_gold, a_pred):

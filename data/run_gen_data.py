163703b881faa72c9f51f13d10455a23bbbe0f97
huayangzhao <921203405@qq.com>
2020-05-27 20:40:04 +0800

    submit

--- data/run_gen_data.py
+++ data/run_gen_data.py
@@ -1,7 +1,22 @@
 import json
 import pickle
-from finetune.qa.squad_official_eval import compute_f1
 import argparse
+
+
+def compute_f1(a_gold, a_pred):
+    gold_toks = get_tokens(a_gold)
+    pred_toks = get_tokens(a_pred)
+    common = collections.Counter(gold_toks) & collections.Counter(pred_toks)
+    num_same = sum(common.values())
+    if len(gold_toks) == 0 or len(pred_toks) == 0:
+        # If either is no-answer, then F1 is 1 if they agree, 0 otherwise
+        return int(gold_toks == pred_toks)
+    if num_same == 0:
+        return 0
+    precision = 1.0 * num_same / len(pred_toks)
+    recall = 1.0 * num_same / len(gold_toks)
+    f1 = (2 * precision * recall) / (precision + recall)
+    return f1
 
 
 def gen_pv_data(std_dev_file, preds_file, output_file):

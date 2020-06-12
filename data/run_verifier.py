f20f0bf4eedefb07ec73b9aff679f5437eb042f3
HuaYZhao <921203405@qq.com>
2020-06-12 15:05:44 +0800

    submit

--- data/run_verifier.py
+++ data/run_verifier.py
@@ -42,7 +42,7 @@ def run_verifier(input_file, data_dir, output_file):
         merge_null_odds[k] = null_odds[k] + pv_null_odds[k]
     json.dump(merge_null_odds, open(tmp_null_odds_file, 'w', encoding='utf-8'))
 
-    xargs = f"python ./data/eval.py {input_file} {preds_file} --na-prob-file {tmp_null_odds_file} --out-file {tmp_eval_file}"
+    xargs = f"python eval.py {input_file} {preds_file} --na-prob-file {tmp_null_odds_file} --out-file {tmp_eval_file}"
     os.system(xargs)
 
     new_sh = json.load(open(tmp_eval_file, 'r', encoding='utf-8'))["best_exact_thresh"]
@@ -52,40 +52,72 @@ def run_verifier(input_file, data_dir, output_file):
             preds[k] = ""
 
     length = 5
-    believe_f1_th = 0.7
-    believe_prob_th = 0.2
-    for qid in preds:
-        chooses = (seq(range(length))
-                   .map(lambda x: answer_candidates_score.get(f"{qid}_{x}", None))
-                   .map(lambda x: x if x is None else {'f1_pred': np.max([y['predictions'] for y in x])})
-                   .zip(dev_all_nbest[qid][:length])
-                   .filter(lambda x: x[0])
-                   .map(lambda x: {'text': x[1]['text'],
-                                   'f1_pred': x[0]['f1_pred'],
-                                   'prob': x[1]['probability']})
-                   .sorted(lambda x: x['f1_pred'], reverse=True)
-                   ).list()
-        if len(chooses):
-            max_prob = seq(chooses).map(lambda x: x['prob']).max()
-            if chooses[0]['f1_pred'] > believe_f1_th and max_prob - chooses[0]['prob'] < believe_prob_th:
-                preds[qid] = chooses[0]['text']
+    believe_f1_th = 1.
+    believe_prob_th = 0.3
+    dev = json.load(open(input_file))
+    for article in dev['data']:
+        for paragraph in article["paragraphs"]:
+            for qa in paragraph['qas']:
+                qid = qa['id']
+                answers = qa['answers']
+                # if not answers:
+                #     print(1)
+                chooses = (seq(range(length))
+                           .map(lambda x: answer_candidates_score.get(f"{qid}_{x}", None))
+                           .map(lambda x: x if x is None else {'f1_pred': np.max([y['predictions'] for y in x])})
+                           .zip(dev_all_nbest[qid][:length])
+                           .filter(lambda x: x[0])
+                           .map(lambda x: {'text': x[1]['text'],
+                                           'f1_pred': x[0]['f1_pred'],
+                                           'prob': x[1]['probability']})
+                           .sorted(lambda x: x['f1_pred'], reverse=True)
+                           ).list()
+                if len(chooses):
+                    max_prob = seq(chooses).map(lambda x: x['prob']).max()
+                    if chooses[0]['f1_pred'] > believe_f1_th and max_prob - chooses[0]['prob'] < believe_prob_th and not preds[qid]:
+                        # if not answers:
+                        #     print(2)
+                        # if not preds[qid] and answers:
+                        #     print(3)
+                        # if answers and preds[qid]:
+                        #     print(4)
+                        preds[qid] = chooses[0]['text']
+                        print(f"{chooses[0]},    {len(answers) > 0}")
+
+    # for qid in preds:
+    #     chooses = (seq(range(length))
+    #                .map(lambda x: answer_candidates_score.get(f"{qid}_{x}", None))
+    #                .map(lambda x: x if x is None else {'f1_pred': np.max([y['predictions'] for y in x])})
+    #                .zip(dev_all_nbest[qid][:length])
+    #                .filter(lambda x: x[0])
+    #                .map(lambda x: {'text': x[1]['text'],
+    #                                'f1_pred': x[0]['f1_pred'],
+    #                                'prob': x[1]['probability']})
+    #                .sorted(lambda x: x['f1_pred'], reverse=True)
+    #                ).list()
+    #     if len(chooses):
+    #         max_prob = seq(chooses).map(lambda x: x['prob']).max()
+    #         if chooses[0]['f1_pred'] > believe_f1_th and max_prob - chooses[0]['prob'] < believe_prob_th and not preds[qid]:
+    #             preds[qid] = chooses[0]['text']
+    #             print(chooses[0])
 
     json.dump(preds, open(output_file, 'w', encoding='utf-8'))
 
-    xargs = f"python ./data/eval.py {input_file} {output_file}"
+    xargs = f"python eval.py {input_file} {output_file}"
     os.system(xargs)
 
 
 def main():
-    parser = argparse.ArgumentParser()
-
-    # Required parameters
-    parser.add_argument('--eval-file', required=True, help="eval file")
-    parser.add_argument('--data-dir', required=True, help="data dir")
-    parser.add_argument('--output-file', required=True, help="final predictions")
-    args = parser.parse_args()
-
-    run_verifier(args.eval_file, args.data_dir, args.output_file)
+    # parser = argparse.ArgumentParser()
+    #
+    # # Required parameters
+    # parser.add_argument('--eval-file', required=True, help="eval file")
+    # parser.add_argument('--data-dir', required=True, help="data dir")
+    # parser.add_argument('--output-file', required=True, help="final predictions")
+    # args = parser.parse_args()
+    #
+    # run_verifier(args.eval_file, args.data_dir, args.output_file)
+    run_verifier("dev-v2.0.json", "./", "pp.json")
 
 
 if __name__ == '__main__':

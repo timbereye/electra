22ba88908d9982b5f857cdec4be65378986457cd
chenxiang <chenxiang@myhexin.com>
2020-07-06 15:23:45 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 DATA_DIR="gs://squad_cx/EData"
-python3 run_finetuning.py --data-dir $DATA_DIR --model-name electra_small --hparams '{"train_batch_size":8, "eval_batch_size":8, "predict_batch_size":8, "model_size": "small", "use_tpu": false, "num_tpu_cores": 8, "tpu_name": "c1","use_tfrecords_if_existing":true, "task_names": ["squad"]}'
+python3 run_finetuning.py --data-dir $DATA_DIR --model-name electra_large --hparams '{"train_batch_size":32, "eval_batch_size":32, "predict_batch_size":32, "model_size": "large", "use_tpu": true, "num_tpu_cores": 8, "tpu_name": "c2","use_tfrecords_if_existing":true, "task_names": ["squad"]}'

05b58fb975c927b1881011ea2315ba9cf21a4e5b
chenxiang <chenxiang@myhexin.com>
2020-07-13 16:03:23 +0800

    att

--- run_small.sh
+++ run_small.sh
@@ -1,2 +1,2 @@
-DATA_DIR="gs://squad_cx/EData_all"
+DATA_DIR="gs://squad_cx/EData"
 python3 run_finetuning.py --data-dir $DATA_DIR --model-name electra_small --hparams '{"train_batch_size":32, "eval_batch_size":32, "predict_batch_size":32, "model_size": "small", "use_tpu": true, "num_tpu_cores": 8, "tpu_name": "c2","use_tfrecords_if_existing":true, "task_names": ["squad"]}'

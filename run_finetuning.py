132601fc1398a5c56063ca6af44aa0574e675e70
chenxiang <chenxiang@myhexin.com>
2020-07-17 13:19:45 +0800

    test

--- run_finetuning.py
+++ run_finetuning.py
@@ -191,7 +191,7 @@ class ModelRunner(object):
         eval_input_fn, _ = self._preprocessor.prepare_predict([task], split)
         results = self._estimator.predict(input_fn=eval_input_fn,
                                           yield_single_examples=True)
-        scorer = task.get_scorer()
+        scorer = task.get_scorer(split=split)
         for r in results:
             if r["task_id"] != len(self._tasks):  # ignore padding examples
                 r = utils.nest_dict(r, self._config.task_names)
